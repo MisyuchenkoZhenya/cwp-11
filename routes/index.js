d077a48fb3ccf086d784ff79683ccc9d8f9b1547
Zhenya <misyuchenko.zhenya@mail.ru>
2017-11-21 10:11:53 +0300

    Task 6 is done

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,10 @@
 const filmRoutes = require('./api/films');
 const actorRoutes = require('./api/actors');
+const express = require('express');
 
 module.exports = (app) => {
     app.use("/api/films", filmRoutes);
     app.use("/api/actors", actorRoutes);
+    app.use("/images/actors", express.static("public/images/actors", { redirect: "/images/error" }));
+    app.use("/images/*", express.static("public/images/error/notFound.gif"));
 }
